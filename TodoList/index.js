ad23441469e95759e35cd79d832162f69edbd20a
Hamza Aman <hamza.ch4040@gmail.com>
2024-08-25 16:34:30 +0500

    Project End

--- TodoList/index.js
+++ TodoList/index.js
@@ -11,11 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("Public"));
 // database init
 const db = new pg.Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "Azmahnama",
-    database: "ToDoList"
+    host: process.env["HOST"] || "localhost",
+    port: process.env["PORT"] || 5432,
+    user: process.env["USER"],
+    password: process.env["PASSWORD"],
+    database: process.env["DB_NAME"],
 });
 db.connect();
 // home route
@@ -24,7 +24,6 @@ app.get('/', async (req, res) => {
     let date = new Date().toISOString().split('T')[0];
     // finding tasks for current date
     let tasks = await db.query('SELECT * FROM tasks where taskdate = $1', [date]);
-    console.log(tasks.rows.filter(t => t.done == false));
     // rendering home page
     res.render("index.ejs", {
         dueTasks: tasks.rows.filter(t => t.done === false),
@@ -35,22 +34,48 @@ app.get('/', async (req, res) => {
 })
 // scheduled tasks route
 app.get('/scheduled', async (req, res) => {
+    // getting tasks sorted by date
     let result = await db.query("SELECT * FROM tasks WHERE done = false ORDER BY taskdate ASC");
-    console.log(result.rows);
-    res.sendStatus(200);
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // days by name
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]; // months by name
+
+    for (let row of result.rows) {
+        // creating a full date value for each task
+        let date = new Date(row.taskdate);
+        row["fullDate"] = `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]}, ${date.getFullYear()}`;
+    }
+    // using the reduce function to map tasks by their date onto an object 
+    let object = result.rows.reduce((obj, row) => {
+        const taskDate = row.fullDate;
+        if (!obj[taskDate]) {
+            obj[taskDate] = [];
+        }
+        obj[taskDate].push(row);
+        return obj;
+    }, {});
+
+    // rendering the page
+    res.render('Scheduled.ejs', {
+        tasks: object,
+        homeActive: false,
+        scheduledActive: true,
+    })
 })
 
-
-
 app.post('/addTask', async (req, res) => {
+    // setting the due time value to be acceptable for the db 
     if (req.body.dueTime === '') req.body.dueTime = null;
-    await db.query('INSERT INTO tasks (taskcontent, tasktime, taskdate, done) VALUES ($1, $2, $3)', [req.body.task, req.body.dueTime, req.body.dueDate, false]);
+    // query for inserting the date to the db
+    await db.query('INSERT INTO tasks (taskcontent, tasktime, taskdate, done) VALUES ($1, $2, $3, $4)', [req.body.task, req.body.dueTime, req.body.dueDate, false]);
+    // redirecting user back to home
     res.redirect('/');
 })
 // Route to check the status of the task if done.
 app.post('/Done', async (req, res) => {
+    // query to change the status of the task
     await db.query('UPDATE tasks SET done = true WHERE id = $1', [req.body.id]);
-    res.redirect('/');
+    // redirecting user back to the page they came from 
+    res.redirect(req.headers.referer);
 })
 // server listening on port 3000
 app.listen(port, (req, res) => {
